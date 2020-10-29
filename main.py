c61ef19c01a7eaeb3e2c09877de84a30003cb8e1
Sheepie <chrisparsonsm97@yahoo.co.uk>
2020-10-29 21:57:20 +0000

    Added a turn counter and the ability to restart the game.

--- main.py
+++ main.py
@@ -8,17 +8,31 @@ x_turn = False
 label = Label(window, text="Naught's Turn")
 label.grid(row=10, column=1)
 buttons = []
+turns = 0
 
 
 def click(row, col):
     global x_turn
     global label
+    global turns
+
     if buttons[row][col]["text"] == "-":
+        turns += 1  # Only increments the turn if the space is valid
         buttons[row][col]["text"] = "X" if x_turn is True else "O"
         x_turn = not x_turn
         label["text"] = "Crosses' turn." if x_turn else "Naught's turn."
     else:
         label["text"] = "Invalid space."
+
+    if turns >= 9:
+        label["text"] = "Game over. Restart?"
+        label.bind("<Button-1>", restart)
+
+
+def restart(event):
+    for row in buttons:
+        for col in row:
+            col["text"] = "-"
 
 
 def setup():
