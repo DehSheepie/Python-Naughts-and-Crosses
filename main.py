4d7bb4333e25fbcb289bd503faff1dd56a277c93
Sheepie <chrisparsonsm97@yahoo.co.uk>
2020-10-30 13:25:28 +0000

    Put the game into a class.

--- main.py
+++ main.py
@@ -1,84 +1,92 @@
 from tkinter import *
 
-window = Tk()
-window.title("Naughts and Crosses")
-x_turn = False
 
-label = Label(window, text="Naught's Turn")
-label.grid(row=10, column=1)
-buttons = []
-turns = 0
+class Game:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Naughts and Crosses")
+        self.x_turn = False
+        self.label = Label(self.window, text="Naught's Turn")
+        self.label.grid(row=10, column=1)
+        self.buttons = []
+        self.turns = 0
+        self.setup()
+        self.end_game_string = ""
+        self.menu = Menu(self.window)
+        self.menu.add_command(label="Restart", command=self.restart)
+        self.window.config(menu=self.menu)
+        self.window.mainloop()
 
+    def run_check(self, li: list):
+        string = ""
+        for space in li:
+            string += self.buttons[space[0]][space[1]]['text']
+        print(string)
+        if string == "XXX" or string == "OOO":
+            self.end_game_string = string
+            return True
+        return False
 
-def run_check(li: list):
-    string = ""
-    for space in li:
-        string += buttons[space[0]][space[1]]['text']
-    print(string)
-    if string == "XXX" or string == "OOO":
-        return True
-    return False
+    def check_win(self):
+        rows = [[0, 0], [0, 1], [0, 2]], [[1, 0], [1, 1], [1, 2]], [[2, 0], [2, 1], [2, 2]]
+        columns = [[0, 0], [1, 0], [2, 0]], [[0, 1], [1, 1], [2, 1]], [[0, 2], [1, 2], [2, 2]]
+        diagonal1 = [[0, 0], [1, 1], [2, 2]]
+        diagonal2 = [[0, 2], [1, 1], [2, 0]]
 
+        if (self.run_check(rows[0])) or (self.run_check(rows[1])) or (self.run_check(rows[2])) \
+                or (self.run_check(columns[0])) or (self.run_check(columns[1])) or (self.run_check(columns[2])) or \
+                (self.run_check(diagonal1)) or (self.run_check(diagonal2)):
+            # self.label["text"] = "Game over. Restart?"
+            if self.end_game_string == "XXX":
+                self.label["text"] = "Nice one Crosses! Restart?"
+            if self.end_game_string == "OOO":
+                self.label["text"] = "Noice Naughts! Restart?"
 
-def check_win():
-    rows = [[0, 0], [0, 1], [0, 2]], [[1, 0], [1, 1], [1, 2]], [[2, 0], [2, 1], [2, 2]]
-    columns = [[0, 0], [1, 0], [2, 0]], [[0, 1], [1, 1], [2, 1]], [[0, 2], [1, 2], [2, 2]]
-    diagonal1 = [[0, 0], [1, 1], [2, 2]]
-    diagonal2 = [[0, 2], [1, 1], [2, 0]]
+            for row in self.buttons:
+                for col in row:
+                    col.bind("<Button-1>", self.restart)
+            self.label.bind("<Button-1>", self.restart)
 
-    if (run_check(rows[0])) or (run_check(rows[1])) or (run_check(rows[2]))\
-            or (run_check(columns[0])) or (run_check(columns[1])) or (run_check(columns[2])) or \
-            (run_check(diagonal1)) or (run_check(diagonal2)):
-        label["text"] = "Game over. Restart?"
-        label.bind("<Button-1>", restart)
+    def click(self, row, col):
+        if self.buttons[row][col]["text"] == "-":
+            self.turns += 1  # Only increments the turn if the space is valid
+            self.buttons[row][col]["text"] = "X" if self.x_turn is True else "O"
+            self.x_turn = not self.x_turn
+            self.label["text"] = "Crosses' turn." if self.x_turn else "Naught's turn."
+        else:
+            self.label["text"] = "Invalid space."
 
+        self.check_win()
 
-def click(row, col):
-    global x_turn
-    global label
-    global turns
+        if self.turns >= 9:
+            self.label["text"] = "A Draw! Restart?"
+            self.label.bind("<Button-1>", self.restart)
 
-    if buttons[row][col]["text"] == "-":
-        turns += 1  # Only increments the turn if the space is valid
-        buttons[row][col]["text"] = "X" if x_turn is True else "O"
-        x_turn = not x_turn
-        label["text"] = "Crosses' turn." if x_turn else "Naught's turn."
-    else:
-        label["text"] = "Invalid space."
-
-    check_win()
-
-    if turns >= 9:
-        label["text"] = "Game over. Restart?"
-        label.bind("<Button-1>", restart)
-
-
-def restart(event):
-    global turns
-    for row in buttons:
-        for col in row:
-            col["text"] = "-"
-    label.unbind("<Button-1>")
-    label["text"] = "Crosses' turn." if x_turn else "Naught's turn."
-    turns = 0
-
-
-def setup():
-    row = 0
-    col = 0
-    while row < 3:
+    def setup(self):
+        row = 0
         col = 0
-        row_values = []
-        while col < 3:
-            # Could not pass in the button as only the last entered values were used
-            # Had to assign r and c to row and col otherwise the values of row and col at the end of the loop were used
-            b = Button(window, text="-", bg="Grey", height=10, width=20, command=lambda r=row, c=col: click(r, c))
-            b.grid(column=col, row=row)
-            row_values.append(b)
-            col += 1
-        buttons.append(row_values)
-        row += 1
+        while row < 3:
+            col = 0
+            row_values = []
+            while col < 3:
+                # Could not pass in the button as only the last entered values were used
+                # Had to assign r and c to row and col otherwise the values of row and col at the end of the loop were used
+                b = Button(self.window, text="-", bg="Grey", height=10, width=20,
+                           command=lambda r=row, c=col: self.click(r, c))
+                b.grid(column=col, row=row)
+                row_values.append(b)
+                col += 1
+            self.buttons.append(row_values)
+            row += 1
+
+    def restart(self, event=None):
+        for row in self.buttons:
+            for col in row:
+                col.unbind("<Button-1>")
+                col["text"] = "-"
+        self.label.unbind("<Button-1>")
+        self.label["text"] = "Crosses' turn." if self.x_turn else "Naught's turn."
+        self.turns = 0
 
 
-setup()
-window.mainloop()
+game = Game()
